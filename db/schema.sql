c771eb2f25f0e6ffce76e9f5f1e707893829ca96
Baljot Hansi <baljotshansi@gmail.com>
2023-10-30 20:08:26 -0300

    Fixed bug and added auto increment to schema.sql.

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE animal_shelter_db;
 USE animal_shelter_db;
 
 CREATE TABLE `animals` (
-  `id` integer PRIMARY KEY,
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
   `name` varchar(255),
   `age` integer,
   `description` text,
@@ -11,7 +11,7 @@ CREATE TABLE `animals` (
 );
 
 CREATE TABLE `adoptions` (
-  `id` integer PRIMARY KEY,
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
   `animal_id` integer,
   `client_id` integer,
   `adoption_status` varchar(255),
@@ -20,7 +20,7 @@ CREATE TABLE `adoptions` (
 );
 
 CREATE TABLE `clients` (
-  `id` integer PRIMARY KEY,
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
   `first_name` varchar(255),
   `last_name` varchar(255),
   `email` varchar(255),
@@ -29,13 +29,13 @@ CREATE TABLE `clients` (
 );
 
 CREATE TABLE `favourites` (
-  `id` integer PRIMARY KEY,
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
   `client_id` integer,
   `animal_id` integer
 );
 
 CREATE TABLE `employees` (
-  `id` integer PRIMARY KEY,
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
   `first_name` varchar(255),
   `last_name` varchar(255),
   `email` varchar(255),
@@ -45,7 +45,7 @@ CREATE TABLE `employees` (
 
 ALTER TABLE `adoptions` ADD FOREIGN KEY (`animal_id`) REFERENCES `animals` (`id`);
 
-ALTER TABLE `clients` ADD FOREIGN KEY (`id`) REFERENCES `adoptions` (`client_id`);
+ALTER TABLE `adoptions` ADD FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`);
 
 ALTER TABLE `favourites` ADD FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`);
 
