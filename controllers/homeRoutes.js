41872fa303c2a46c971a082f5a33403fc4902176
Baljot Hansi <baljotshansi@gmail.com>
2023-11-01 20:52:26 -0300

    Added homepage controller and index for routing. Coded root, dogs, and cats routes into the homepage. Added handlebars files for the routes also.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,9 +6,41 @@ router.get('/', async (req, res) => {
         const animalsData = await Animals.findAll();
         const animals = animalsData.map((animal) => animal.get({ plain: true }));
 
-        res.render('homepage', { animals });
+        res.render('homepage', { ...animals });
     }
     catch(err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/dogs', async (req, res) => {
+    try {
+        const dogData = await Animals.findAll({
+            where: {
+                species: 'dog'
+            }
+        });
+        const dogs = dogData.map((dog) => dog.get({ plain: true }));
+
+        res.render('dogs', { ...dogs });
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/cats', async (req, res) => {
+    try {
+        const dogData = await Animals.findAll({
+            where: {
+                species: 'cat'
+            }
+        });
+        const cats = dogData.map((cat) => cat.get({ plain: true }));
+
+        res.render('cats', { ...cats });
+    }
+    catch (err) {
         res.status(500).json(err);
     }
 });
