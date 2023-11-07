3094305e8179393a4618efa871ff15d68568f09a
Baljot Hansi <baljotshansi@gmail.com>
2023-11-06 20:42:48 -0400

    Added session variables to responses in home routes. Renamed employeeRoutes.js to userRoutes.js, removed animal get routes from the file, and added login and logout routes.

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,51 @@
+const router = require('express').Router();
+const { Employees, Clients } = require('../../models');
+
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await (
+        req.body.is_employee ?
+        Employees            :
+        Clients              ).findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res.status(400).json({
+        message: 'Incorrect email or password, please try again.'
+      });
+      return;
+    }
+
+    const validPassword = await employeeData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({
+        message: 'Incorrect email or password, please try again.'
+      });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      req.session.is_employee = req.body.is_employee;
+
+      res.json({
+        user: userData,
+        message: 'You are now logged in!'
+      });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,7 +6,11 @@ router.get('/', async (req, res) => {
     const animalsData = await Animals.findAll();
     const animals = animalsData.map((animal) => animal.get({ plain: true }));
 
-    res.render('homepage', { ...animals });
+    res.render('homepage', {
+      ...animals,
+      logged_in: req.session.logged_in,
+      is_employee: req.session.is_employee
+    });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -21,7 +25,11 @@ router.get('/dogs', async (req, res) => {
     });
     const dogs = dogData.map((dog) => dog.get({ plain: true }));
 
-    res.render('dogs', { ...dogs });
+    res.render('homepage', {
+      ...dogs,
+      logged_in: req.session.logged_in,
+      is_employee: req.session.is_employee
+    });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -29,16 +37,30 @@ router.get('/dogs', async (req, res) => {
 
 router.get('/cats', async (req, res) => {
   try {
-    const dogData = await Animals.findAll({
+    const catData = await Animals.findAll({
       where: {
         species: 'cat',
       },
     });
-    const cats = dogData.map((cat) => cat.get({ plain: true }));
+    const cats = catData.map((cat) => cat.get({ plain: true }));
 
-    res.render('cats', { ...cats });
+    res.render('homepage', {
+      ...cats,
+      logged_in: req.session.logged_in,
+      is_employee: req.session.is_employee
+    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
 module.exports = router;
