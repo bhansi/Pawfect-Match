876b6fd7392e54928cca9273b66faa8541c09167
Kirill777-web <ki.lazutin@gmail.com>
2023-11-08 18:45:54 -0400

    added: modified handlebars files added conditions and partials added: modified server.js added: modified homeRoutes.js ('/', login , adoption routes)

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,17 +1,23 @@
 const router = require('express').Router();
 const { Animals } = require('../models');
 
+//Home route
 router.get('/', async (req, res) => {
   try {
     const animalsData = await Animals.findAll();
     const animals = animalsData.map((animal) => animal.get({ plain: true }));
 
-    res.render('homepage', { ...animals });
+    // Render a single page
+    res.render('animals', {
+      showNavBar: true, // condition to show the nav bar
+      animals, // Pass the animals data to the template
+      title: 'Animals Page', // Pass the title to the template
+    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
-
+//Dogs route
 router.get('/dogs', async (req, res) => {
   try {
     const dogData = await Animals.findAll({
@@ -26,7 +32,7 @@ router.get('/dogs', async (req, res) => {
     res.status(500).json(err);
   }
 });
-
+//Cats route
 router.get('/cats', async (req, res) => {
   try {
     const dogData = await Animals.findAll({
@@ -40,5 +46,17 @@ router.get('/cats', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
+});
+
+//Adoption form route
+router.get('/adoptions', (req, res) => {
+  res.render('adoptions', { title: 'Adoptions Page' });
+  res.render('adoptions', { showNavBar: false });
+});
+
+//Login route
+router.get('/login', (req, res) => {
+  res.render('login', { title: 'Login Page' });
+  res.render('login', { showNavBar: false }); // condition to not show the nav bar
 });
 module.exports = router;
