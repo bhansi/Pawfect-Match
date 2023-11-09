a12bae7ac34a84f3e4ec48ce9a108d27e61365e1
Baljot Hansi <baljotshansi@gmail.com>
2023-11-08 20:47:14 -0400

    Moved employee specific routes from userRoutes to employeeRoutes.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const employeeRoutes = require('./employeeRoutes');
 
+router.use('/employee', employeeRoutes);
 router.use('/user', userRoutes);
 
 module.exports = router;
