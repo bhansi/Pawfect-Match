9c034256900d81aa1066d18969120280435d38aa
Baljot Hansi <baljotshansi@gmail.com>
2023-11-11 21:48:15 -0400

    Created client route and added logic to retrieve all active applications for a given logged in client.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const employeeRoutes = require('./employeeRoutes');
+const clientRoutes = require('./clientRoutes');
 
-router.use('/employee', employeeRoutes);
 router.use('/user', userRoutes);
+router.use('/employee', employeeRoutes);
+router.use('/client', clientRoutes);
 
 module.exports = router;
